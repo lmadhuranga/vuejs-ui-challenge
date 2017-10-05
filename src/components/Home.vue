c1fd9fa8202165004a4f724d5eb3a3aadb322c3f
lmaduranga <lilan.maduranga@gmail.com>
2017-10-05 17:03:36 +0530

    Basic person list ok

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,24 +12,33 @@
             </v-btn>
           </v-toolbar>
           <v-list>
-            <v-list-group v-for="item in items" :value="item.active" v-bind:key="item.title">
+            <v-list-group :value="true" v-bind:key="0">
               <v-list-tile slot="item" @click="">
                 <v-list-tile-action>
-                  <v-icon>{{ item.action }}</v-icon>
+                  <v-icon>restaurant</v-icon>
                 </v-list-tile-action>
                 <v-list-tile-content>
-                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-title>Users</v-list-tile-title>
                 </v-list-tile-content>
                 <v-list-tile-action>
                   <v-icon>keyboard_arrow_down</v-icon>
                 </v-list-tile-action>
               </v-list-tile>
-              <v-list-tile v-for="subItem in item.items" v-bind:key="subItem.title" @click="">
+
+              <v-list-tile v-bind:key="10000" @click="">
                 <v-list-tile-content>
-                  <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
+                  <v-list-tile-title><router-link :to="{ name: 'add'}">Add Person</router-link></v-list-tile-title>
                 </v-list-tile-content>
                 <v-list-tile-action>
-                  <v-icon>{{ subItem.action }}</v-icon>
+                  <v-icon>school</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+              <v-list-tile v-for="person in people" v-bind:key="person.name" @click="">
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ person.name }}</v-list-tile-title>
+                </v-list-tile-content>
+                <v-list-tile-action>
+                  <!--<v-icon>{{ subItem.action }}</v-icon>-->
                 </v-list-tile-action>
               </v-list-tile>
             </v-list-group>
@@ -42,56 +51,6 @@
 
 <script>
     export default {
-        data () {
-            return {
-                items: [
-                    {
-                        action: 'restaurant',
-                        title: 'Dining',
-                        active: true,
-                        items: [
-                            { title: 'Breakfast & brunch' },
-                            { title: 'New American' },
-                            { title: 'Sushi' }
-                        ]
-                    },
-                    {
-                        action: 'school',
-                        title: 'Education',
-                        items: [
-                            { title: 'List Item' }
-                        ]
-                    },
-                    {
-                        action: 'directions_run',
-                        title: 'Family',
-                        items: [
-                            { title: 'List Item' }
-                        ]
-                    },
-                    {
-                        action: 'healing',
-                        title: 'Health',
-                        items: [
-                            { title: 'List Item' }
-                        ]
-                    },
-                    {
-                        action: 'content_cut',
-                        title: 'Office',
-                        items: [
-                            { title: 'List Item' }
-                        ]
-                    },
-                    {
-                        action: 'local_offer',
-                        title: 'Promotions',
-                        items: [
-                            { title: 'List Item' }
-                        ]
-                    }
-                ]
-            }
-        }
+        props: ['people']
     }
 </script>
