d41c7f8eb055038c1ce698e45bfb50a465f86a9e
lmadhuranga <lilan.maduranga@gmail.com>
2017-10-31 17:23:43 +0000

    Improve unite test

--- test/templates/FloorManagersListMenu.spec.js
+++ test/templates/FloorManagersListMenu.spec.js
@@ -1,15 +1,15 @@
 import Vue from 'Vue';
-import SelectedShift from '../../src/components/template/ProjectListMenu.vue'
+import FloorManagersListMenu from '../../src/components/template/FloorManagersListMenu.vue'
 import router from '../../src/router'
 import data from '../../src/config/data'
 import {shallow, mount} from "vue-test-utils"
 import VueRouter from 'vue-router';
 
-describe('FloorManagersListMenu.vue', () => {
+describe('FloorMangersListMenu.vue', () => {
   Vue.use(VueRouter);
-  let cmp, user, shift, projectId, userId, shiftId, shiftIndex, project;
+  let cmp, floorManager;
 
-  const createCmp = propsData => shallow(SelectedShift, {
+  const createCmp = propsData => shallow(FloorManagersListMenu, {
     propsData,
     el: document.createElement('div'),
     router: router,
@@ -17,31 +17,18 @@ describe('FloorManagersListMenu.vue', () => {
   });
 
   beforeEach(() => {
-    projectId = 1;
-    shiftIndex = 1;
-    shift = data.projects[projectId].shifts[0];
-    shiftId = shift.id;
-    project = data.projects[projectId];
+    floorManager = data.users[0];
 
     cmp = createCmp({
-      projects: {1: project},
-      selectedproject: {id: projectId}
+      floorManagers: [floorManager]
     });
   });
 
-  it('should be able load projects and selectedProject', () => {
-    expect(cmp.vm.selectedproject.id).toEqual(projectId);
-    expect(cmp.vm.projects[1]).toEqual(project);
+  it('should be able load floorManagers', () => {
+    expect(cmp.vm.floorManagers[0]).toEqual(floorManager);
   });
 
   it('has the expected html structure', () => {
     expect(cmp.element).toMatchSnapshot()
-  })
-
-  // it('should be project list show in the list', () => {
-  //   expect(cmp.vm.content).to.contain
-  // });
-  it('should be button click then trigger selectedShift function', () => {
-    // TODO cmp.vm.selectedShift = jest.fn();
   });
 });
